afb572edc09fcdf3ecf59de76263407aa2de3e5f
byronwasti <byron.wasti@gmail.com>
2016-12-01 10:38:12 -0500

    Implementing qubit and not gate

--- app/Main.hs
+++ app/Main.hs
@@ -4,3 +4,4 @@ import Lib
 
 main :: IO ()
 main = quantumRun
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -5,4 +5,10 @@ module Lib
 import Numeric.LinearAlgebra
 
 quantumRun :: IO ()
-quantumRun = putStrLn "Some Func"
+quantumRun = do putStrLn "Some Func"
+                putStrLn $ show $ notGate (Qubit 2 3)
+
+data Qubit a = Qubit a a deriving Show
+
+notGate :: (Fractional a) => Qubit a -> Qubit a
+notGate (Qubit x y) = Qubit (-x) (-y)
