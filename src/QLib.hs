740923f892bb7dabdf5958849710a1b2f7f513e0
byronwasti <byron.wasti@gmail.com>
2016-12-12 11:07:31 -0500

    Implemented with hmatrix

--- src/QLib.hs
+++ src/QLib.hs
@@ -1,44 +1,66 @@
 module QLib
     ( quantumRun,
-      Qubit(..)
+      Qubit(..),
     ) where
 
 import Data.Complex
+import Data.List
+import Text.Printf
+import Numeric.LinearAlgebra
 
 quantumRun :: IO ()
 quantumRun = do putStrLn "Some Func"
-                let c = Qubit (1 :+ 0) (0 :+ 0)
+                putStrLn $ printf "%b" (3 :: Int)
+                let c = Qubit $ fromList [1 :+ 0, 0]
                 putStrLn $ show c
-                let d = sauliY c
+                let d = sauliX c
                 putStrLn $ show d
-                let e = sauliZ d
+                let e = Register $ take 5 $ repeat (Qubit $ fromList [1, 0])
+                --let e = sauliZ d
                 putStrLn $ show e
 
+data Qubit = Qubit (Vector C)
+data Register = Register [Qubit]
+
+instance Show Qubit where
+    show (Qubit vect) = show vect
+
+instance Show Register where
+    show (Register xs) = concatMap show xs
+
+sauliX :: Qubit -> Qubit
+sauliX (Qubit x) = Qubit (xgate #> x)
+                where xgate = (2><2)[0, 1, 1, 0] :: Matrix C
+
+sauliY :: Qubit -> Qubit
+sauliY (Qubit x) = Qubit (ygate #> x)
+                where ygate = (2><2)[0:+1, 0, 0, 0:+1] :: Matrix C
+
+sauliZ :: Qubit -> Qubit
+sauliZ (Qubit x) = Qubit (zgate #> x)
+                where zgate = (2><2)[1, 0, 0, -1] :: Matrix C
                 
 
-data Bit = Zero | One deriving Eq
+--data Bit = Zero | One deriving Eq
+--
+--instance Show Bit where
+--    show Zero = "|0>"
+--    show One  = "|1>"
+--
+--data Qubit a = Qubit (Complex a) (Complex a) deriving Show
+--
+--sauliX :: Qubit a -> Qubit a
+--sauliX (Qubit x y) = Qubit y x
+--
+--sauliY :: (Fractional a, RealFloat a) => Qubit a -> Qubit a
+--sauliY (Qubit x y) = Qubit rotY rotX
+--        where i = 0 :+ 1
+--              rotY = -y*i
+--              rotX = x*i
+--
+--sauliZ :: (RealFloat a) => Qubit a -> Qubit a
+--sauliZ (Qubit x y) = Qubit x (-y)
 
-instance Show Bit where
-    show Zero = "|0>"
-    show One  = "|1>"
-
-data Qubit a = Qubit (Complex a) (Complex a) deriving Show
-
-sauliX :: Qubit a -> Qubit a
-sauliX (Qubit x y) = Qubit y x
-
-sauliY :: (Fractional a, RealFloat a) => Qubit a -> Qubit a
-sauliY (Qubit x y) = Qubit rotY rotX
-        where i = 0 :+ 1
-              rotY = -y*i
-              rotX = x*i
-
-sauliZ :: (RealFloat a) => Qubit a -> Qubit a
-sauliZ (Qubit x y) = Qubit x (-y)
-
-hadamard :: (RealFloat a) => Qubit a -> Qubit a
-hadamard (Quibit x y) = Qubit hadX hadY
-         where hadX =
 
 
 --data Qubit a = Qubit (Vector a)
